3e78a4aea124a3e0010a29e84ecc8fe38e60fb7f
JotaPrimo <gestald118@gmail.com>
2023-06-26 20:45:44 -0400

    72. Criando componentes do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import './App.css'
-import StartScreen from './components/StartScreen'
+// css
+import "./App.css";
 
-function App() {  
+// react
+import { useCallback, useEffect, useState } from "react";
+
+// import data
+import {wordsList} from "./data/words";
+
+// comporntes
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+
+  // CONSTS
+  const START = "start";
+  const GAME = "game";
+  const END = "end";
+
+  // functions check
+  const isStart = () => {
+    return gameStage === START;
+  };
+
+  const isGame = () => {
+    return gameStage === GAME;
+  };
+
+  const isEnd = () => {
+    return gameStage === END;
+  };
+  // functions check
+
+
+  // inicializando o stageGame
+  const [gameStage, setStageGame] = useState(stages[0].name);
+  const [words] = useState(wordsList)
 
   return (
-    <div className='App'>
-      <StartScreen />
+    <div className="App">
+      {isStart && <StartScreen />}
+      {isGame === "game" && <Game />}
+      {isEnd === "end" && <GameOver />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
